5ddeb3d13a543aaa125f6342c0ccf65d2e874d8c
Seff <46768740+seffs@users.noreply.github.com>
2023-02-04 02:40:30 +0100

    refactor: dotfiles

--- modules/home-manager/macos.nix
+++ modules/home-manager/macos.nix
@@ -5,7 +5,7 @@
     FONTCONFIG_FILE = "~/.config/fontconfig";
   };
   xdg.configFile."ytfzf" = {
-    source = ./config/ytfzf/conf.sh;
+    source = ./config/ytfzf/conf-darwin.sh;
     target = "ytfzf/conf.sh";
   };
   programs.zsh.enable = true;
