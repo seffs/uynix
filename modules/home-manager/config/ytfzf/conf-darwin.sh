a29e890d1f05cc322f5374ee6fce01d41889adfd
Seff <46768740+seffs@users.noreply.github.com>
2023-06-03 02:12:51 +0200

    ytfzf: fallback to macports installation

--- modules/home-manager/config/ytfzf/conf-darwin.sh
+++ modules/home-manager/config/ytfzf/conf-darwin.sh
@@ -13,5 +13,5 @@ video_player () {
     command_exists "mpv" || die 3 "mpv is not installed\n"
     [ "$is_detach" -eq 1 ] && use_detach_cmd=detach_cmd || use_detach_cmd=''
     # shellcheck disable=SC2086
-    $use_detach_cmd /usr/local/bin/mpv --ytdl-format="$ytdl_pref" $url_handler_opts "$@"
+    $use_detach_cmd /opt/local/bin/mpv --ytdl-format="$ytdl_pref" $url_handler_opts "$@"
 }
